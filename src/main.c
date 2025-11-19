cefba197018b4119229c2c363204f1d6e0bed890
ElyesLaaribi <elyeslaaribii@gmail.com>
2025-11-19 23:48:29 +0100

    capturing users command

--- src/main.c
+++ src/main.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
+  // printing $ sign at the beginning of the shell
   printf("$ ");
+
+  // captures the users command
+  char input[100];
+  fgets(input, sizeof(input), stdin);
+  input[strcspn(input, "\n")] = '\0';
+  printf("%s: command not found\n", input);
 
   return 0;
 }
